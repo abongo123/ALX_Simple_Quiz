6eb85d553ecd4f3d92b852cf20a885fc85287eea
abongo123 <103944706+abongo123@users.noreply.github.com>
2025-01-19 03:41:23 -0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,47 @@
+//addition
+function add(number1, number2) {
+    return number1 + number2;
+}
+//substraction
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+//Multiplication
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+//division
+function divide(number1, number2) {
+    return number1 / number2;
+}
+//addition
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
 
+//substraction
+document.getElementById('subtract').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = subtract(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+//multyplication
+document.getElementById('multiply').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = multiply(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+//Division
+document.getElementById('divide').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = divide(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
